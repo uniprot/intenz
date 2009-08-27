23f07431e08037460e897796366213d2eb3528e5
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2009-08-27 15:33:43 +0000

    fixed db configuration issues

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,20 +4,35 @@
 # Parameters:
 #   $1 - [optional] configuration files directory
 
+if [ -z $INTENZ_CONFIG_DIR ]
+then
+    echo "Set variable INTENZ_CONFIG_DIR to a directory containing database configuration"
+    exit 1
+fi
+
 EXPORT_HOME=`dirname $0`/..
 CONFIG_DIR=${1:-$EXPORT_HOME/src/main/appResources}
 INTENZ_DIR=$EXPORT_HOME/..
-INTENZ_PUBLIC_DIR=$INTENZ_DIR/webapps/public/src/main/resources
+INTENZ_PUBLIC_DIR=$INTENZ_DIR/webapp/intenz-public
+
+SITEMAP_OUT_DIR=`grep 'intenz.export.sitemap.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
+[ -d $SITEMAP_OUT_DIR ] || mkdir -p $SITEMAP_OUT_DIR
+
+BIOPAX_OUT_DIR=`grep 'intenz.export.biopax.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
+[ -d $BIOPAX_OUT_DIR ] || mkdir -p $BIOPAX_OUT_DIR
+
+XML_OUT_DIR=`grep 'intenz.export.xml.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
+[ -d $XML_OUT_DIR ] || mkdir -p $XML_OUT_DIR
 
 cd $EXPORT_HOME
-cvs update
+svn update
 mvn clean package
 
-PROXY_OPTS=$(grep 'ebi.proxy.java.options' $CONFIG_DIR/ebi-proxy.properties | cut -d '=' -f 2)
+PROXY_OPTS=$(grep 'ebi.proxy.java.options' $INTENZ_CONFIG_DIR/ebi-proxy.properties | cut -d '=' -f 2)
 
 MEM_OPTS="-Xmx512M"
 
-CP=$CONFIG_DIR:$INTENZ_DIR:$INTENZ_PUBLIC_DIR
+CP=$CONFIG_DIR
 for JAR in $EXPORT_HOME/target/*.jar
 do
     CP=$CP:$JAR
@@ -25,14 +40,11 @@ done
 
 java $PROXY_OPTS $MEM_OPTS -cp $CP uk.ac.ebi.intenz.tools.export.ExporterApp
 
-SITEMAP_OUT_DIR=`grep 'intenz.export.sitemap.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
 gzip $SITEMAP_OUT_DIR/sitemap.xml
-cp $SITEMAP_OUT_DIR/sitemap.xml.gz ../../webapps/public/src/main/webapp
+cp $SITEMAP_OUT_DIR/sitemap.xml.gz $INTENZ_PUBLIC_DIR/src/main/webapp
 
-BIOPAX_OUT_DIR=`grep 'intenz.export.biopax.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
 gzip $BIOPAX_OUT_DIR/intenz-biopax.owl
 
-XML_OUT_DIR=`grep 'intenz.export.xml.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
 cp $EXPORT_HOME/src/main/resources/{intenz,enzyme}.xsd $XML_OUT_DIR
 cp $EXPORT_HOME/src/main/resources/IntEnzXML-README.txt $XML_OUT_DIR
 cd $XML_OUT_DIR
