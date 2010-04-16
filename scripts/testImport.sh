88771878b03dc9eed3a0a47894ea32a6a1549a83
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2010-04-16 10:28:55 +0000

    v1.0.5

--- scripts/testImport.sh
+++ scripts/testImport.sh
@@ -2,9 +2,9 @@
 #
 # Import external cross-references
 # Parameters:
-#   $1 xrefs to be imported, one of GO or UNIPROT (default: all)
+#   $1 - xrefs to be imported, one of GO or UNIPROT (default: all)
 
-SCRIPT_DIR=`dirname $0`
+SCRIPT_DIR=$(dirname $0)
 CONFIG_DIR=$SCRIPT_DIR/../src/test/appResources
 
 $SCRIPT_DIR/doImport.sh $CONFIG_DIR $@
