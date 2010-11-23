5e1237b6754526d9704212e621899bf8fa12dd5b
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2010-11-23 13:34:27 +0000

    added exporter for KEGG enzyme format

--- intenz-export/scripts/export.sh
+++ intenz-export/scripts/export.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 #
 # Exports IntEnz production database in all formats.
+# Parameters:
+#   Those for ExporterApp (see javadocs)
 
-EXPORT_HOME=`dirname $0`/..
-CONFIG_DIR=$EXPORT_HOME/src/main/appResources
+`dirname $0`/doExport.sh $@
 
-SITEMAP_OUT_DIR=`grep 'intenz.export.sitemap.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
-[ -d $SITEMAP_OUT_DIR ] || mkdir -p $SITEMAP_OUT_DIR
-
-BIOPAX_OUT_DIR=`grep 'intenz.export.biopax.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
-[ -d $BIOPAX_OUT_DIR ] || mkdir -p $BIOPAX_OUT_DIR
-
-XML_OUT_DIR=`grep 'intenz.export.xml.output.dir' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
-[ -d $XML_OUT_DIR ] || mkdir -p $XML_OUT_DIR
-
-EXPORT_FORMAT=`grep 'intenz.export.format' $CONFIG_DIR/intenz-export.properties | cut -d '=' -f 2`
-
-`dirname $0`/doExport.sh $CONFIG_DIR
+[ $? == 0 ] || exit 1
 
 cd $EXPORT_HOME
 
-if [ "${EXPORT_FORMAT#*SITEMAP*}" != "$EXPORT_FORMAT" ]
-then
-	gzip $SITEMAP_OUT_DIR/sitemap.xml
-fi
-
-if [ "${EXPORT_FORMAT#*BIOPAX*}" != "$EXPORT_FORMAT" ]
-then
-	gzip $BIOPAX_OUT_DIR/intenz-biopax.owl
-fi
-
-if [ "${EXPORT_FORMAT#*INTENZ_XML*}" != "$EXPORT_FORMAT" ]
-then
-	cp $EXPORT_HOME/../intenz-xml/src/main/resources/{intenz,enzyme}.xsd $XML_OUT_DIR
-	cp $EXPORT_HOME/src/main/resources/IntEnzXML-README.txt $XML_OUT_DIR
-	cd $XML_OUT_DIR
-	zip -r IntEnzXML.zip *
-fi
+# Parse parameters sent to ExporterApp in order to compress files:
+for PARAM in $@
+do
+    case "$LAST_OPT" in
+        '-sitemap'|'biopax'|'keggEnzyme')
+			gzip $PARAM
+			;;
+        '-intenzXml')
+			cp $EXPORT_HOME/../intenz-xml/src/main/resources/{intenz,enzyme}.xsd $PARAM
+			cp $EXPORT_HOME/src/main/resources/IntEnzXML-README.txt $PARAM
+			cd $PARAM
+			zip -r IntEnzXML.zip *
+			;;
+    esac
+    case $PARAM in
+        '-intenzXml'|'-sitemap'|'biopax'|'keggEnzyme') LAST_OPT=$PARAM ;;
+        *) unset LAST_OPT ;;
+    esac
+done
