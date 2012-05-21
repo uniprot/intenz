fc6ac090ab4defe1f22de8b2478dbf7e3a48f7e8
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-05-21 10:24:44 +0000

    Back to clean code (no IUBMB flag in the trunk yet).

--- scripts/testUpdateIntEnzText.sh
+++ scripts/testUpdateIntEnzText.sh
@@ -2,4 +2,4 @@
 #
 # Test the update of IntEnzText database table
 
-`dirname $0`/updateIntEnzText.sh `dirname $0`/../src/test/appResources
+$(dirname $0)/updateIntEnzText.sh $(dirname $0)/../src/test/appResources
