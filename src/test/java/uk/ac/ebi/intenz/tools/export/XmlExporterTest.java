c5a5929578d848a84780916dd8dfcddae96391dc
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-10-03 12:00:33 +0000

    v1.3.11

--- src/test/java/uk/ac/ebi/intenz/tools/export/XmlExporterTest.java
+++ src/test/java/uk/ac/ebi/intenz/tools/export/XmlExporterTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.sql.Connection;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,61 +17,52 @@ import uk.ac.ebi.intenz.mapper.EnzymeEntryMapper;
 public class XmlExporterTest {
 
 	private XmlExporter exporter;
+	private Connection con;
+	private EnzymeEntryMapper mapper;
+	private final Logger LOGGER = Logger.getLogger(XmlExporterTest.class);
 	
 	@Before
 	public void setUp() throws Exception {
 		exporter = new XmlExporter();
+		con = OracleDatabaseInstance.getInstance("intenz-db-dev")
+				.getConnection();
+		mapper = new EnzymeEntryMapper();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (con != null) con.close();
 	}
 
 	@Test
 	public void testExportEnzymeEntryStringStringOutputStream()
 	throws Exception {
-        EnzymeEntryMapper mapper = new EnzymeEntryMapper();
-        Connection con = null;
 		try {
-			con = OracleDatabaseInstance.getInstance("intenz-db-dev").getConnection();
 	        EnzymeEntry entry = mapper.findById(1000L, con);
 			exporter.export(entry, "9999", "9999-09-09", System.out);
 		} catch (IOException e) {
 			fail(e.getMessage());
-		} finally {
-			if (con != null) con.close();
 		}
 	}
 
 	@Test
 	public void testExportEnzymeEntryNoLinks()
 	throws Exception {
-        EnzymeEntryMapper mapper = new EnzymeEntryMapper();
-        Connection con = null;
 		try {
-			con = OracleDatabaseInstance.getInstance("intenz-db-dev").getConnection();
 	        EnzymeEntry entry = mapper.findById(17515L, con);
 			exporter.export(entry, "9999", "9999-09-09", System.out);
 		} catch (IOException e) {
 			fail(e.getMessage());
-		} finally {
-			if (con != null) con.close();
 		}
 	}
 	
 	@Test
 	public void testExportEnzymeEntry() throws Exception{
-        EnzymeEntryMapper mapper = new EnzymeEntryMapper();
-        Connection con = null;
 		try {
-			con = OracleDatabaseInstance.getInstance("intenz-db-dev")
-					.getConnection();
 	        EnzymeEntry entry = mapper.findById(10396L, con);
 			exporter.export(entry, "9999", "9999-09-09", System.out);
 		} catch (IOException e) {
 			fail(e.getMessage());
-		} finally {
-			if (con != null) con.close();
 		}
 	}
 
