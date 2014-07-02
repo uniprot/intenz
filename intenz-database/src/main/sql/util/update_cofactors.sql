998e9d5932a99b417c2fa96ec8366bdad8867225
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2014-07-02 08:45:47 +0000

    Removing unused compounds; clearer ChEBI IDs; final report.

--- intenz-database/src/main/sql/util/update_cofactors.sql
+++ intenz-database/src/main/sql/util/update_cofactors.sql
@@ -1,9 +1,19 @@
+/*
+Updates IntEnz cofactors (name, ChEBI ID, formula, charge) with any changes in
+ChEBI. Sends the report to stdout.
+*/
+
 SET SERVEROUTPUT ON;
 
 DECLARE
   remCofactors number := 0;
+  updatedChebiIds number := 0;
+  updatedNames number := 0;
+  updatedFormulae number := 0;
+  updatedCharges number := 0;
   theChebiId number;
   newName varchar2(4000);
+  newNameSrc varchar2(512);
   newChemData varchar2(512);
 BEGIN
   /* Remove any compounds which are not used as cofactors: */
@@ -14,7 +24,9 @@ BEGIN
     --DBMS_OUTPUT.PUT_LINE(reg.compound_id || ' should be removed from COMPOUND_DATA table');
     remCofactors := remCofactors + 1;
   end loop;
-  DBMS_OUTPUT.PUT_LINE('Compounds to remove: ' || remCofactors);
+  if (remCofactors > 0) then
+    delete from compound_data where compound_id = reg.compound_id;
+  end if;
   
   /* Update remaining compounds (cofactors): */
   for reg in (SELECT
@@ -29,39 +41,45 @@ BEGIN
     if (reg.parent_id is null) then
       theChebiId := reg.c_id;
     else
-      DBMS_OUTPUT.PUT_LINE(reg.c_id || ' *MERGED* to ' || reg.parent_id);
+      DBMS_OUTPUT.PUT_LINE('CHEBI:' || reg.c_id
+        || ' *MERGED* to ' || 'CHEBI:' || reg.parent_id);
       theChebiId := reg.parent_id;
-      -- TODO: update the ChEBI ID
+      update compound_data set accession = theChebiId
+        where accession = reg.c_id;
+      updatedChebiIds := updatedChebiIds + 1;
     end if;
     
     -- Compare names:
     begin
-      select cn.name into newName
+      select cn.name, cn.source into newName, newNameSrc
         from chebi.names@chezpro cn, chebi.compounds@chezpro cc
         where (cc.id in (reg.c_id, theChebiId) or cc.parent_id = theChebiId)
         and cc.id = cn.compound_id
-        and cn.source in ('UniProt','ChEBI')
         and cn.type = 'NAME'
+        and cn.source in ('UniProt','ChEBI')
         and cn.status in ('C','S')
         order by cn.source desc, cn.status asc;
       if (newName != reg.i_name) then
-        -- TODO: update name
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' *name CHANGED* from ' || reg.i_name
-          || ' to ' || newName);
-      else
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' name unchanged: ' || reg.i_name);
+        DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+          || ' *name CHANGED* from ' || reg.i_name
+          || ' to ' || newName || ' (source: ' || newNameSrc || ')');
+        update compound_data set name = newName
+          where accession = 'CHEBI:'||theChebiId;
+        updatedNames := updatedNames + 1;
       end if;
     exception
       when no_data_found then
         if (reg.i_name != reg.c_name) then
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' *name ERROR* - was: ' || reg.i_name
-            || '; found in ChEBI: ' || reg.c_name );
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' *name not found* - was: ' || reg.i_name
+            || '; found in CHEBI.COMPOUND: ' || reg.c_name );
         else
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' name (ChEBI) unchanged: '
-            || reg.i_name);
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' name (CHEBI.COMPOUND) unchanged: ' || reg.i_name);
         end if;
       when others then
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' name *ERROR* - ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+          || ' name *ERROR* - ' || SQLERRM);
     end;
     
     -- Formula:
@@ -71,23 +89,24 @@ BEGIN
         where (cc.id in (reg.c_id, theChebiId) or cc.parent_id = theChebiId)
         and cc.id = cd.compound_id
         and cd.type = 'FORMULA'
-        and cd.status in ('C','S');
+        and cd.status in ('C','S') order by cd.status asc;
       if (reg.i_formula != newChemData) then
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' *formula CHANGED* from '
-            || reg.i_formula || ' to ' || newChemData );
-          -- TODO: update formula
-      else
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' formula unchanged: ' || reg.i_formula);
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' *formula CHANGED* from ' || reg.i_formula
+            || ' to ' || newChemData );
+          update compound_data set formula = newChemData
+            where accession = 'CHEBI:'||theChebiId;
+          updatedFormulae := updatedFormulae + 1;
       end if;
     exception
       when no_data_found then
         if (reg.i_formula is not null) then
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' *formula VANISHED*: ' || reg.i_formula);
-        else
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' formula unchanged: ' || reg.i_formula);
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' *formula VANISHED*: ' || reg.i_formula);
         end if;
       when others then
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' *formula ERROR* - ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+          || ' *formula ERROR* - ' || SQLERRM);
     end;
     
     -- Charge:
@@ -97,24 +116,33 @@ BEGIN
         where (cc.id in (reg.c_id, theChebiId) or cc.parent_id = theChebiId)
         and cc.id = cd.compound_id
         and cd.type = 'CHARGE'
-        and cd.status in ('C','S');
+        and cd.status in ('C','S') order by cd.status asc;
       if (reg.i_charge != newChemData) then
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' *charge CHANGED* from '
-            || reg.i_charge || ' to ' || newChemData );
-          -- TODO: update charge
-      else
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' charge unchanged: ' || reg.i_charge);
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' *charge CHANGED* from ' || reg.i_charge
+            || ' to ' || newChemData );
+          update compound_data set charge = to_number(newChemData)
+            where accession = 'CHEBI:'||theChebiId;
+          updatedCharges := updatedCharges + 1;
       end if;
     exception
       when no_data_found then
         if (reg.i_charge != 0) then
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' *charge VANISHED*: ' || reg.i_charge);
-        else
-          DBMS_OUTPUT.PUT_LINE(theChebiId || ' charge unchanged: ' || reg.i_charge);
+          DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+            || ' *charge VANISHED*: ' || reg.i_charge);
         end if;
       when others then
-        DBMS_OUTPUT.PUT_LINE(theChebiId || ' *charge ERROR* - ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('CHEBI:' || theChebiId
+          || ' *charge ERROR* - ' || SQLERRM);
     end;
   end loop;
+  
+  DBMS_OUTPUT.PUT_LINE('');
+  DBMS_OUTPUT.PUT_LINE('Unused cofactors removed: ' || remCofactors);
+  DBMS_OUTPUT.PUT_LINE('Updated ChEBI IDs: ' || updatedChebiIds);
+  DBMS_OUTPUT.PUT_LINE('Updated names: ' || updatedNames);
+  DBMS_OUTPUT.PUT_LINE('Updated formulae: ' || updatedFormulae);
+  DBMS_OUTPUT.PUT_LINE('Updated charges: ' || updatedCharges);
+
 END;
 /
