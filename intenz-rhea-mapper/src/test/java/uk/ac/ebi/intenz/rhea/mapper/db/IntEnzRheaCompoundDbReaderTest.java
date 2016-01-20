db9049c2eb0940deceea6bf6cd8d50cf45998676
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-01-20 16:41:11 +0000

    Updating Intenz : 	- dependency updates 	- added uniprot jAPI 	- unit test updates 	- pom updates 	- db config updates

--- intenz-rhea-mapper/src/test/java/uk/ac/ebi/intenz/rhea/mapper/db/IntEnzRheaCompoundDbReaderTest.java
+++ intenz-rhea-mapper/src/test/java/uk/ac/ebi/intenz/rhea/mapper/db/IntEnzRheaCompoundDbReaderTest.java
@@ -41,7 +41,7 @@ public class IntEnzRheaCompoundDbReaderTest {
 //                userHome + "/tns_admin");
 
         SQLLoader sQLLoader = SQLLoader.getSQLLoader(SQL_FILE);
-        instance = new IntEnzRheaCompoundDbReader(sQLLoader.getConnection(), sQLLoader);
+        instance = new IntEnzRheaCompoundDbReader(SQLLoader.getConnection(), sQLLoader);
     }
 
     @After
