df0a59acef430cf0060cb6be07569adf4a135b2d
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-01-26 14:57:41 +0000

    Updating Intenz-DB Indexing : 	-update java -X option to -Xmx6G

--- intenz-dbIndexing/scripts/updateIntEnzText.sh
+++ intenz-dbIndexing/scripts/updateIntEnzText.sh
@@ -5,6 +5,6 @@
 [ ! -d 'logs' ] && mkdir logs
 
 echo "${DB_USER}@${DB_INSTANCE} Updating table INTENZ_TEXT..."
-java -cp $CP uk.ac.ebi.intenz.tools.release.IntEnzText $TARGET_DB
+java -Xmx6G -cp $CP uk.ac.ebi.intenz.tools.release.IntEnzText $TARGET_DB
 echo 'exit' | sqlplus ${DB_USER}/${DB_PASSWD}@${DB_INSTANCE} \
 	@$DBINDEXING_HOME/src/main/sql/intenz_text
