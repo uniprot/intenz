43561cd5ce2b1b31d032f9739409f6760ab51ff7
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-02-10 11:27:26 +0000

    Updating Intenz database indexing : 	- update the java memory to -Xmx6G

--- intenz-dbIndexing/scripts/updateId2Ec.sh
+++ intenz-dbIndexing/scripts/updateId2Ec.sh
@@ -5,4 +5,4 @@
 [ ! -d 'logs' ] && mkdir logs
 
 echo "${DB_USER}@${DB_INSTANCE} Updating table ID2EC..."
-java -cp $CP uk.ac.ebi.intenz.tools.release.ID2EC $TARGET_DB
+java -Xmx6G -cp $CP uk.ac.ebi.intenz.tools.release.ID2EC $TARGET_DB
